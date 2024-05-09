164d1ce6ce6105381801c495f4fdfcf61e9d59d8
0renu <orenams01@gmail.com>
2024-05-09 21:03:54 +0200

    SFSZD

--- src/shaders/fragment_shader.glsl
+++ src/shaders/fragment_shader.glsl
@@ -30,7 +30,8 @@ void main(){
     vec3 L;
     for (int i = 0 ; i < numberOfLight; i++){
         float specular=0.0;
-        float diffuse=0.0;
+        float diffuse
+        =0.0;
         vec3 hwd , vue ;
         float anglespec;
         if (is_directional[i] == 1)
